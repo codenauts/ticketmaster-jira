6a26918f27f8c7fac7a85ac547f71a72c846dd80
Rafael George <george.rafael@gmail.com>
2011-05-11 18:52:27 -0400

    added search of projects by array of id spec

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -7,6 +7,7 @@ describe "TicketMaster::Provider::Jira::Project" do
     @project = Struct.new(:id, :name, :description).new(1, 'project', 'project description')
     Jira4R::JiraTool.stub!(:new).with(2, @url).and_return(@fj)
     @fj.stub!(:getProjectsNoSchemes).and_return([@project, @project])
+    @fj.stub!(:getProjectById).and_return(@project)
     @tm = TicketMaster.new(:jira, :username => 'testuser', :password => 'testuser', :url => @url)
     @klass = TicketMaster::Provider::Jira::Project
   end
@@ -16,7 +17,12 @@ describe "TicketMaster::Provider::Jira::Project" do
     @tm.projects.first.should be_an_instance_of(@klass)
   end
 
-  it "should be able to load all projects based on an array of id's"
+  it "should be able to load all projects based on an array of id's" do 
+    @tm.projects([1]).should be_an_instance_of(Array)
+    @tm.projects.first.should be_an_instance_of(@klass)
+    @tm.projects.first.id.should == 1
+  end
+
   it "should be able to load all projects by attributes" 
   it "should be able to load a single project based on id" 
   it "should be able to load a single project by attributes"
