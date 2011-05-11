4148ec0dcf9b9ef8a9ba371589134f4c31677eff
Rafael George <george.rafael@gmail.com>
2011-05-11 18:35:19 -0400

    change rspec display is better with some colors

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TicketMaster::Provider::Jira::Project" do 
-  it "should be able to load all projects" 
+  before(:each) do
+    @url = "some_url"
+    @fj = FakeJiraTool.new
+    @project = Struct.new(:name, :description)
+    Jira4R::JiraTool.stub!(:new).with(2, @url).and_return(@fj)
+    @fj.stub!(:getProjectsNoSchemes).and_return([@project, @project])
+    @tm = TicketMaster.new(:jira, :username => 'testuser', :password => 'testuser', :url => @url)
+    @klass = TicketMaster::Provider::Jira::Project
+  end
+
+  it "should be able to load all projects" do
+    @tm.projects.should be_an_instance_of(Array)
+    @tm.projects.first.should be_an_instance_of(@klass)
+  end
+
   it "should be able to load all projects based on an array of id's"
   it "should be able to load all projects by attributes" 
   it "should be able to load a single project based on id" 
